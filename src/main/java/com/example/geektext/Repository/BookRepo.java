aee71ff2407ed85ca838dc5b951b236b1de755a7
amarcano27 <amarc122@fiu.edu>
2024-04-09 17:04:30 -0400

    Implement Wishlist Feature with Associated Classes and Interfaces
    - Add `User` and `UserRepo` to manage user entities and database interactions.
    - Introduce `Wishlist`, `WishlistController`, `WishlistItem`, and `WishlistRepo` for handling wishlist functionality.
    - Create `WishlistService` to encapsulate business logic for wishlist operations.
    - Implement `WishlistServiceTest` to ensure reliability and correctness of the wishlist service methods.
    - Include `WishlistItemRepo` for database operations related to wishlist items.
    
    This commit lays the foundation for the Wishlist feature, allowing users to create, update, and manage their wishlists effectively. It includes the necessary entities, repositories, service logic, and tests to ensure a robust implementation.

--- src/main/java/com/example/geektext/Repository/BookRepo.java
+++ src/main/java/com/example/geektext/Repository/BookRepo.java
@@ -1,12 +1,12 @@
 package com.example.geektext.Repository;
-import com.example.geektext.Entity.Books;
+
+import com.example.geektext.Entity.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
 
-
-@Repository
-public interface BookRepo extends JpaRepository<Books, String>{
-   
-
+public interface BookRepo extends JpaRepository<Book, String> {
+    List<Book> findByGenre(String genre);
+    Optional<Book> findByIsbn(String isbn);
 }
